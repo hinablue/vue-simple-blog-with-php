6fa41a7ca920d54bb5a90448d2a4c292784c9208
Hina Chen <hinablue@gmail.com>
2017-09-07 15:14:53 +0800

    Add uploader method and add new method for files model

--- api/model/files.php
+++ api/model/files.php
@@ -6,5 +6,26 @@ class Files extends \Blog\Model {
 
     protected $root = '00000000-9999-2222-3333-000000000000';
 
+    public function add($data) {
+        $this->db->beginTransaction();
+        try {
+            $id = $this->uuid($this->root, microtiem());
 
+            $statement = $this->db->prepare('INSERT INTO `files` (`id`,`user_id`,`filename`,`url`) VALUES (:id,:user_id,:filename,:url)');
+            $statement->bindParam(':id', $id, \PDO::PARAM_STR);
+            $statement->bindParam(':user_id', $this->app->auth['id'], \PDO::PARAM_STR);
+            $statement->bindParam(':filename', $data['filename'], \PDO::PARAM_STR);
+            $statement->bindParam(':url', $data['url'], \PDO::PARAM_STR);
+            if (false === $statement->execute()) {
+                $this->db->rollBack();
+            }
+            $this->db->commit();
+            return [
+                'id' => $id,
+                'url' => $data['url']
+            ];
+        } catch (\PDOException $e) {
+            return false;
+        }
+    }
 }
